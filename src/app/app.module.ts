797ced5f63e74419cd89a6873fdaedbb50e0c0eb
Brian Christopher Mendoza <78596685+briancgmendoza@users.noreply.github.com>
2021-09-12 19:50:58 +0800

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './components/form/form.component';
+import { ButtonComponent } from './components/button/button.component';
+import { FormGroupComponent } from './components/form-group/form-group.component';
+import { DropdownComponent } from './components/dropdown/dropdown.component';
+import { RadioButtonComponent } from './components/radio-button/radio-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    ButtonComponent,
+    FormGroupComponent,
+    DropdownComponent,
+    RadioButtonComponent
   ],
   imports: [
     BrowserModule,
